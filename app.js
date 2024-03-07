77681e7f3de750a9a46ab4da4920767b20935cf4
Almeidaisa <125597235+Almeidaisa@users.noreply.github.com>
2024-03-07 14:18:59 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function App() {
+ const [count, setCount] = useState(0);
+ const [count2, setCount2] = useState(0); 
+ const incrementCount = () => {
+    setCount(count + 1);
+ };
+
+ const incrementCount2 = () => { 
+    setCount2(count2 + 1);
+ };
+
+ return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador em React Native</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Button title="Incrementar" onPress={incrementCount} />
+      <Text style={styles.count}>{count2}</Text> 
+      <Button title="Incrementar 2" onPress={incrementCount2} /> 
+    </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+ },
+ title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+ },
+ count: {
+    fontSize: 30,
+    marginBottom: 20,
+ },
+});
+	
